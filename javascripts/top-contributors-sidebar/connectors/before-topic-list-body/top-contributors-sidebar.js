b5b91425382b5d027c27ae20c9597ae1e3af6316
Jean <jmperez127@gmail.com>
2023-07-18 15:10:44 -0400

    FEATURE: sort by likes given and add groups exclusion (#8)

--- javascripts/top-contributors-sidebar/connectors/before-topic-list-body/top-contributors-sidebar.js
+++ javascripts/top-contributors-sidebar/connectors/before-topic-list-body/top-contributors-sidebar.js
@@ -1,32 +1,43 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
+const fetchDirectoryItems = (settings, component) => {
+  if (!shouldFetchDirectoryItems(settings, component)) {
+    return;
+  }
+
+  const requestURL = `/directory_items.json?period=yearly&order=${settings.order_by}&exclude_groups=${settings.excluded_group_names}&limit=5`;
+
+  fetch(requestURL)
+    .then((response) => response.json())
+    .then((data) => {
+      component.set("hideSidebar", false);
+      component.set("topContributors", data.directory_items);
+    });
+};
+
+const shouldFetchDirectoryItems = (settings, component) => {
+  return (
+    settings.enable_top_contributors &&
+    component.discoveryList &&
+    !component.isDestroyed &&
+    !component.isDestroying
+  );
+};
+
 export default {
   setupComponent(attrs, component) {
     component.set("hideSidebar", true);
     document.querySelector(".topic-list").classList.add("with-sidebar");
 
-    if (!this.site.mobileView) {
-      withPluginApi("0.11", (api) => {
-        api.onPageChange(() => {
-          if (settings.enable_top_contributors) {
-            if (this.discoveryList) {
-              if (this.isDestroyed || this.isDestroying) {
-                return;
-              }
-              component.set("isDiscoveryList", true);
-
-              fetch(`/directory_items.json?period=yearly&order=likes_received`)
-                .then((response) => response.json())
-                .then((data) => {
-                  component.set("hideSidebar", false);
-                  this.set("topContributors", data.directory_items.slice(0, 5));
-                });
-            } else {
-              component.set("isDiscoveryList", false);
-            }
-          }
-        });
-      });
+    if (this.site.mobileView) {
+      return;
     }
+
+    withPluginApi("0.11", (api) => {
+      api.onPageChange(() => {
+        component.set("isDiscoveryList", !!this.discoveryList);
+        fetchDirectoryItems(settings, component);
+      });
+    });
   },
 };
